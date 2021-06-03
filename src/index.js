d1427cf071a47dd927e10721ec4527d21d4c9732
beckzayi <beckzayi@126.com>
2021-06-03 17:29:52 +1000

    install webpack, webpack-cli. npx webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * 1. 运行指令:
+ *      开发环境: npx webpack ./src/index.js -o ./build --mode=development
+ *      生产环境: npx webpack ./src/index.js -o ./build --mode=production
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 2));
