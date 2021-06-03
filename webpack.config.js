9dc80d7bcd11333c3f556a4c2c9dfc9c10f6b570
beckzayi <beckzayi@126.com>
2021-06-04 02:10:59 +1000

    HtmlWebpackPlugin, generate html files with webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,5 +7,13 @@ module.exports = {
     filename: "built.js",
     path: resolve(__dirname, "build"),
   },
+  plugins: [
+    // 功能: 默认会创建一个空的HTML, 自动引入打包输出的所有资源 (JS/CSS)
+    // 需求: 需要有结构的HTML文件
+    new HtmlWebpackPlugin({
+      // 复制该index.html文件, 并自动引入打包输出所有资源 (JS/CSS)
+      template: resolve(__dirname, "src", "index.html"),
+    }),
+  ],
   mode: "development",
 };
