d52cc605d9a81f9a823b529e63932f91c4f290d5
beckzayi <beckzayi@126.com>
2021-06-05 00:47:51 +1000

    js output location set in a specific js folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "built.js",
+    filename: "js/built.js",
     path: resolve(__dirname, "build"),
   },
   module: {
